49ab10c76316a5127de410d56a65639842bcd720
Nik497926 <2000-nikitos@bk.ru>
2024-11-30 11:58:32 +0300

    1

--- module/scripts.js
+++ module/scripts.js
@@ -3,12 +3,17 @@ document.querySelectorAll('.spoiler-button').forEach((button) => {
         const allSpoilers = document.querySelectorAll('.spoiler-content');
         const currentSpoiler = button.nextElementSibling;
 
-        // Скрыть все открытые спойлеры
+        // Скрыть все другие спойлеры
         allSpoilers.forEach((spoiler) => {
             if (spoiler !== currentSpoiler) {
                 spoiler.style.display = 'none';
             }
         });
+
+        // Проверка на наличие скриншотов
+        if (currentSpoiler.getAttribute('data-has-screenshots') === 'false') {
+            currentSpoiler.innerHTML = '<p>Без скринов</p>';
+        }
 
         // Переключение видимости текущего спойлера
         currentSpoiler.style.display =
